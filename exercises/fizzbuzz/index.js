4886845e37ef4deb9dc1d4fa9b5b2b73b2f0f190
MarySalemme <marysalemme1@gmail.com>
2018-05-16 17:08:11 +0100

    FizzBuzz solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,23 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  var i = 1;
+  while (i <= n) {
+    if ((i % 5 === 0) & (i % 3 === 0)) {
+      console.log("fizzbuzz");
+      i++;
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+      i++;
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+      i++;
+    } else {
+      console.log(i);
+      i++;
+    }
+  }
+}
 
 module.exports = fizzBuzz;
