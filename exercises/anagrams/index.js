bfa9fc8ed71cfdf0d08103a1f4ec9fcc480c38bd
MarySalemme <marysalemme1@gmail.com>
2018-05-22 14:22:23 +0100

    Anagrams solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const convertString = string => {
+    return string
+      .replace(/( )/g, '')
+      .toLowerCase()
+      .split('');
+  };
+
+  const stringAChars = convertString(stringA);
+  const stringBChars = convertString(stringB);
+
+  return (
+    stringAChars.length == stringBChars.length &&
+    stringAChars.sort().join('') === stringBChars.sort().join('')
+  );
+}
 
 module.exports = anagrams;
+
+// const fn = s =>
+//   [...s].reduce((acc, char) => {
+//     if (acc.hasOwnProperty(char)) return { ...acc, [char]: acc[char] + 1 };
+//     return { ...acc, [char]: 1 };
+//   }, {});
+
+// const mapA = fn(stringA);
+
+// const mapB = fn(stringB);
+
+// return Object.entries(mapA).every(([key, occurance]) => {
+//   return mapB[key] == occurance;
+// });
